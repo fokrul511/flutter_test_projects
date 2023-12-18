ae081c44a36ba2b5105895db4b62fd79469f32f2
fokrul511 <islamfokrul2002@gmail.com>
2023-12-18 08:30:25 +0600

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,43 +5,140 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        leading: const Icon(
-          Icons.home,
-          color: Colors.white,
-        ),
-        actions: const [
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Icon(
-              Icons.menu,
-              color: Colors.white,
+    var textStyle = TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+      color: Colors.cyan,
+    );
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('flutter apps'),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.menu),
             ),
-          ),
-        ],
-        title: const Text(
-          'Test Projects',
-          style: TextStyle(color: Colors.white),
+          ],
         ),
-        centerTitle: true,
-        backgroundColor: Colors.red,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Center(
-            child: Text('Test Projects'),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Click'),
-          ),
-        ],
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Text(
+                  'Flutter projects',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  '1',
+                  style: textStyle,
+                ),
+                Text(
+                  '3',
+                  style: textStyle,
+                ),
+                Text(
+                  '4',
+                  style: textStyle,
+                ),
+                Text(
+                  '5',
+                  style: textStyle,
+                ),
+                Text(
+                  '6',
+                  style: textStyle,
+                ),
+                Text(
+                  '7',
+                  style: textStyle,
+                ),
+                Text(
+                  '8',
+                  style: textStyle,
+                ),
+                Text(
+                  '9',
+                  style: textStyle,
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Fokrul Islam',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Image(
+              image: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/73265474?v=4'),
+              width: 200,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Fokrul Islam',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Image(
+              image: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/73265474?v=4'),
+              width: 200,
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('simple Projecs'),
+                        content: Text('about this projecs apps'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {},
+                            child: Text('Cancel'),
+                          ),
+                        ],
+                      );
+                    });
+              },
+              child: Text('tap here'),
+            ),
+            SizedBox(height: 10),
+            Spacer(),
+            //RichText.....
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RichText(
+                text: TextSpan(
+                  text: '@copyright by: ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.red,
+                  ),
+                  children: [
+                    TextSpan(
+                        text: 'fokrulIslam@.com ',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        )),
+                    // TextSpan(text: 'Ostad',style: TextStyle(color: Colors.blueAccent))
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
