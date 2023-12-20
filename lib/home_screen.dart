2582626e93dfc11b4f5182cc3a2b8a4c27f0b0d8
fokrul511 <islamfokrul2002@gmail.com>
2023-12-20 13:09:57 +0600

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -23,20 +23,28 @@ class HomePage extends StatelessWidget {
             )
           ],
         ),
-        body: Column(
-          children: [
-            Text(
-              ' today witch wiget implement our apps',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Colors.red,
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(image: NetworkImage('https://img.'
+                'freepik.com/free-vector/gradient-liquid-mobille-wa'
+                'llpaper_79603-461.jpg?size=6'
+                '26&ext=jpg&ga=GA1.1.1546980028.1702684800&semt=ais'), fit: BoxFit.cover)
+          ),
+          child: Column(
+            children: [
+              Text(
+                ' today witch wiget implement our apps',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
               ),
-            ),
-            Row(
-              children: [],
-            ),
-          ],
+              Row(
+                children: [],
+              ),
+            ],
+          ),
         ),
       ),
     );
